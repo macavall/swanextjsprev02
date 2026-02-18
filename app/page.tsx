c5b68e0ced4907b6e203933b6bb4da4a6331f160
Matthew <macavall@microsoft.com>
2026-02-18 15:26:14 -0600

    first

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,9 @@ export default function Home() {
       <div>
         This is a Next.js application hosted on Azure Static Web Apps with hybrid rendering. The time on the server is <strong>{timeOnServer}</strong>.
       </div>
+      <div>
+        TEST_VARIABLE value: <strong>{process.env.NEXT_PUBLIC_TEST_VARIABLE}</strong>
+      </div>
       <CurrentTimeFromAPI />
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
